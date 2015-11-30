c85b493267bc093baf32add8e1a256252b8d6980
Nivlek273 <whitek273@gmail.com>
2015-11-30 12:17:32 -0500

    More additions
    Added intro paragraph. Altered TableFix description. Added more to
    Internship description. Added footer. Numerous styling changes.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
-$(document).ready(function (){
+$(document).ready(function () {
 	$('.dropdown').Dropdown();
-	
-	$('#apply-tFix').click(function(){
-		var counter = 3 * $('table').length;
-		while ($('table:not(".no-tableFix")').length && counter > 0) {
-			tableFix($($('table:not(".no-tableFix")')[0]));
-			counter--;
+
+	$('#apply-tFix').click(function () {
+		if (!$(this).hasClass('disabled')) {
+			$(this).addClass('disabled');
+			var counter = 3 * $('table').length;
+			while ($('table:not(".no-tableFix")').length && counter > 0) {
+				tableFix($($('table:not(".no-tableFix")')[0]));
+				counter--;
+			}
+
 		}
 	})
-	
+
 });
